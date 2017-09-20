d6f2409f377f0b8b8953beef632a230119f4b431
porryli <468168931@qq.com>
2017-09-20 15:03:56 +0800

    first commit

---
+++ thrift-server/src/main/java/com/server/service/QueryImp.java
@@ -0,0 +1,20 @@
+package com.server.service;
+
+import com.query.QryResult;
+import com.query.TestQry;
+import org.apache.thrift.TException;
+
+public class QueryImp implements TestQry.Iface {
+    @Override
+    public QryResult qryTest(int qryCode) throws TException {
+        QryResult result = new QryResult();
+        if(qryCode==1){
+            result.code = 1;
+            result.msg = "success";
+        }else{
+            result.code = 0;
+            result.msg = "fail";
+        }
+        return result;
+    }
+}
